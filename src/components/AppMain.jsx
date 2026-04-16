f1d218a8a72b63b375b5953c93aaee96d2c1c8a3
salvatore tullo <salvatoretullo15@gmail.com>
2026-04-16 17:46:47 +0200

    aggiunto al componente main la barra nera e la barra blu

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,8 +2,44 @@ export default function AppMain(){
 
 
   return (
-    <main> 
-      Main
+    <main className="bg-black text-white"> 
+      <div className="container p-5">
+        <p>--CONTENT GOES HERE--</p>
+      </div>
+      <div className="bg-primary p-5">
+        <div className="justify-content-center text-center align-items-center row">
+          <div className="col-md-2">
+            <img src="./public/img/buy-comics-digital-comics.png" alt="digital comics"/>
+            <span>
+              DIGITAL COMICS
+            </span>
+          </div>
+          <div className="col-md-2">
+            <img src="./public/img/buy-comics-merchandise.png" alt="merchandise"/>
+            <span>
+              MERCHANDISE
+            </span>
+          </div>
+          <div className="col-md-2">
+            <img src="./public/img/buy-comics-shop-locator.png" alt="shop locator"/>
+            <span>
+              SHOP LOCATOR
+            </span>
+          </div>
+          <div className="col-md-2">
+            <img src="./public/img/buy-comics-subscriptions.png" alt="subscriptions"/>
+            <span>
+              SUBSCRIPTION
+            </span>
+          </div>
+          <div className="col-md-2">
+            <img src="./public/img/buy-dc-power-visa.svg" alt="dc power visa"/>
+            <span>
+              DC POWER VISA
+            </span>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
