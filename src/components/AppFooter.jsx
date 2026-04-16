282f7a8b8f7e25b957bbb17ddb26f135c5a11f28
salvatore tullo <salvatoretullo15@gmail.com>
2026-04-16 18:26:23 +0200

    aggiunta sezione crediti

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -2,8 +2,112 @@ export default function AppFooter(){
 
 
   return (
-    <footer> 
-      
+    <footer className="jumbotron"> 
+    <div className="container justify-content-center text-white">
+      <div className="row">
+          <div className="col-sm-1 col-md-2">
+            <h2>
+              DC COMICS
+            </h2>
+            <ul>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+              <li>
+                <a>characters</a>
+              </li>
+            </ul>
+            <h2>
+              SHOP
+            </h2>
+            <ul>
+              <li>
+                <a>shop</a>
+              </li>
+              <li>
+                <a>shop</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-1 col-md-2">
+            <h2>
+              DC
+            </h2>
+            <ul>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+              <li>
+                <a>dc</a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-sm-1 col-md-2">
+            <h2>
+              SITES
+            </h2>
+            <ul>
+              <li>
+                <a>sites</a>
+              </li>
+              <li>
+                <a>sites</a>
+              </li>
+              <li>
+                <a>sites</a>
+              </li>
+              <li>
+                <a>sites</a>
+              </li>
+              <li>
+                <a>sites</a>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </div>
     </footer>
   )
 }
