058737ab9be6dd884d788f6186e013f55362996f
salvatore tullo <salvatoretullo15@gmail.com>
2026-04-16 16:25:50 +0200

    commit apertura esercizio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import AppFooter from "./components/AppFooter"
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
+
+function App() {
+  
+
+  return (
+    <>
+      <AppHeader/>
+      <AppMain/>
+      <AppFooter/>
+    </>
+  )
+}
+
+export default App
